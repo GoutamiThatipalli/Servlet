fb1dc9d85ae188342f13e0f6d236576acc5aa996
goutamit <goutami.thatipalli@imaginea.com>
2017-08-24 10:03:45 +0530

    commit

---
+++ src/main/java/controller/EditServlet2.java
@@ -0,0 +1,76 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.student;
+import service.StudentDAO;
+
+/**
+ * Servlet implementation class EditServlet2
+ */
+public class EditServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditServlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");  
+        PrintWriter out=response.getWriter();  
+          
+        String s=request.getParameter("userid");  
+        String p=request.getParameter("password");  
+        String m=request.getParameter("mobileno");  
+        String e=request.getParameter("email");  
+        String c=request.getParameter("city");  
+        String pi=request.getParameter("pincode");  
+          
+        student st=new student();  
+        st.setUserid(s);  
+        st.setPassword(p);  
+        st.setMobileno(m);  
+        st.setEmail(e);
+        st.setCity(c);
+        st.setPincode(pi);   
+        
+        int status;
+		try {
+			status = StudentDAO.updateStudent(st);
+			if(status>0){  
+	            response.sendRedirect("ViewServlet");  
+	        }else{  
+	            out.println("Sorry! unable to update record");  
+	        }  
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}  
+        
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
